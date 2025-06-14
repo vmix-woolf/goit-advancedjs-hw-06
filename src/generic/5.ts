4b0a8eaa8988858090c4792d8bc48caa9b20b6b4
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2025-06-14 08:29:22 +0300

    add second four generic tasks

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -1,11 +1,21 @@
 /*
-  Вам потрібно реалізувати інтерфейс KeyValuePair, який описує пару ключ-значення. 
+  Вам потрібно реалізувати інтерфейс KeyValuePair, який описує пару ключ-значення.
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<K, V> {
+  key: K;
+  value: V;
 }
+
+const pair1: KeyValuePair<string, number> = {
+  key: 'id',
+  value: 1001
+};
+
+const pair2: KeyValuePair<number, boolean> = {
+  key: 42,
+  value: false
+};
 
 export {};
